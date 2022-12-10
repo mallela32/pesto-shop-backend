8e6a27d6bf54ee5bede8439ed915f39eebf4f19f
nitheesh <mnc9505@gmail.com>
2022-12-10 23:35:37 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ const app = express();
 //   next();
 // });
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
